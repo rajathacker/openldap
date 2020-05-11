703a143a5ccf5104085092d5f9caa4a6ad458122
Rajat Agrawal <ragrawal@redhat.com>
2020-05-11 10:07:12 +0530

    modified user_generator.sh

--- Lab_3/Solutions/user_generator.sh
+++ Lab_3/Solutions/user_generator.sh
@@ -5,7 +5,7 @@ echo "Creating users and groups for HR organizational unit"
 echo ""
 echo ""
 
-~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=hr,dc=example,dc=com -n 10006 > ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
+~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=hr,dc=example,dc=com -n 10024 > ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
 
 echo "Modifying User and IDs"
 sed -i s/10000/21000/g ~/openldap/Lab_3/gen_dit/gen_dit.py
@@ -16,7 +16,7 @@ echo "Creating users and groups for IT organization unit"
 echo ""
 echo ""
 
-~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=it,dc=example,dc=com -n 10006 >> ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
+~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=it,dc=example,dc=com -n 10024 >> ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
 
 echo "Modifying Used and Group IDs"
 sed -i s/21000/32000/g ~/openldap/Lab_3/gen_dit/gen_dit.py
@@ -27,7 +27,7 @@ echo "Creating users and groups for Sales organization unit"
 echo ""
 echo ""
 
-~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=sales,dc=example,dc=com -n 10006 >> ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
+~/openldap/Lab_3/gen_dit/gen_dit.py -s ou=sales,dc=example,dc=com -n 10024 >> ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif ; sed -i  '1,/cn=hr/!d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif  ; sed -i  '$d' ~/openldap/Lab_3/gen_dit/automated_user_ldif_generator.ldif
 
 
 echo ""
